3bb7e5677b480915a65a8fdb8f723f0332a8676a
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-05-16 07:29:59 -0600

    🪲 ADD .env to netilfy

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,3 +1,10 @@
 const fs = require("fs");
 
-fs.writeFileSync("../.env", `API=${process.env.API}\n`);
+let data = `API=${process.env.API}`;
+
+fs.writeFile("./.env", data, (err) => {
+	if (err) console.log(err);
+	else {
+		console.log("File written successfully\n");
+	}
+});
